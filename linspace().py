511310da44f9419ebb3bba2289615c0351b18e41
Yukti Manoj Mulani <ymulani@myseneca.ca>
2023-11-15 12:42:00 -0500

    final commit

---
+++ linspace().py
@@ -0,0 +1,5 @@
+import numpy as np
+x=np.linspace(10,30,10)#generates 10 random parameters between 10 and 30
+print(x)
+# [10.         12.22222222 14.44444444 16.66666667 18.88888889 21.11111111
+#  23.33333333 25.55555556 27.77777778 30.        ]
