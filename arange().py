f395e7ee840150b129199d0600b8b2d1fc74c11a
Yukti Manoj Mulani <ymulani@myseneca.ca>
2023-11-15 12:26:00 -0500

    final commit

---
+++ arange().py
@@ -0,0 +1,4 @@
+import numpy as np
+np.arange(5)# retuns an array with numbers upto 5
+example=np.arange(3,10,dtype="float")# an array starting from 3 and till 10 this is kind of like the range function for numpy arrays
+print(example)
