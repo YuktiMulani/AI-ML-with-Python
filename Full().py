9b61fe7d515dca5ca9b2a024f9f9dc0a3835f3a7
Yukti Manoj Mulani <ymulani@myseneca.ca>
2023-11-15 11:25:45 -0500

    final commit

---
+++ Full().py
@@ -0,0 +1,5 @@
+import numpy as np
+x=np.full(8,5)# 8 is shape and 5 is the value of elements.
+print(x)
+y=np.full((2,3),6,dtype="float")# 2 rows and 3 clomns one dimensional array with value of all elements as 6
+print(y)
