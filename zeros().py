6576ffd0487a6b65a113be3269be1eb2454f7f16
Yukti Manoj Mulani <ymulani@myseneca.ca>
2023-11-15 10:53:53 -0500

    final commit

---
+++ zeros().py
@@ -0,0 +1,4 @@
+import numpy as np
+np.zeros(5)#shape = size of the array
+np.zeros((3,5),dtype="int")# 3 rows and 5 columns
+# this will create an array with 3 rows and five columns that will be a 2 dimensional array.
